184ab0423afb72438b4dcc2763aab44c76c0b35a
Author <geroliabryan024@gmail.com>
2025-09-11 18:09:59 +0800

    added some comments and edit the columns

---
+++ app/Models/Patient.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Models is basically what interacts with the DB
+ Dito nakalagay yung mga code na may Query 
+
+ May different ways para makipag-interact sa DB such as 
+ Raw queries, Query builder, or the Eloquent ORM
+
+ Make sure to note na gamit natin is Eloquent ORM para 
+ same-same tayo at hindi nakakaconfuse basahin yung code. 
+
+*/
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Patient extends Model
+{
+    //
+}
+
+

0d8b108dc8ad4f84282af38cf6e40131126c4a4a
Author <geroliabryan024@gmail.com>
2025-09-11 18:24:23 +0800

    added some comments again in the controller and model

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,18 @@
+<?php
+
+/* TO:DO
+[] Adding Patient
+[] Removing Patient
+[] Updating Patient
+[] Read Patient Data (All and Specific Patient)
+ */
+
+namespace App\Http\Controllers;
+
+use App\Models\Patient;
+use Illuminate\Http\Request;
+
+class PatientController extends Controller
+{
+    //
+}

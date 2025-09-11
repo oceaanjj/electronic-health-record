7932a39741f7978b03f5f397dd45a84ab6a02666
oceaanjj <jovi.uccsc@gmail.com>
2025-09-11 19:30:10 +0800

    adding home page & css (not finish)

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/home-style.css',  'resources/js/app.js'],
-            
+            input: ['resources/css/app.css', 
+                    'resources/css/home-style.css',
+                    'resources/css/login-style.css',
+                    'resources/css/registration-style.css',
+                    'resources/js/app.js'],
+
             refresh: true,
         }),
         tailwindcss(),

e02e6060fae8753884e4b5479165aedc0fed6008
oceaanjj <jovi.uccsc@gmail.com>
2025-09-15 00:40:23 +0800

    ADL, IVs & Lines, Lab Values HTML structure

---
+++ resources/views/ivs-and-lines.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Patient Activities of daily living</title>
+    @vite(['./resources/css/#.css'])
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <label for="patient">PATIENT NAME :</label>
+            <select id="patient" name="patient">
+                <option value="">-- Select Patient --</option>
+                <option value="Althea Pascua">Jovilyn Esquerra</option>
+            </select>
+        </div>
+
+
+        <table>
+                    <tr>
+                        <th class="title">IV FLUID</th>
+                        <th class="title">RATE</th>
+                        <th class="title">SITE</th>
+                        <th class="title">STATUS</th>
+                    </tr>
+
+                    <tr>
+                        <td><input type="text" placeholder="iv fluid"></td>
+                        <td><input type="text" placeholder="rate"></td>
+                        <td><input type="text" placeholder="site"></td>
+                        <td><input type="text" placeholder="status"></td>
+                    </tr>
+        
+
+
+                  
+                   
+        </table>
+    </div>
+
+    <div class="btn">
+        <button type="submit">Submit</button>
+    </div>
+
+    
+</body>
+</html>

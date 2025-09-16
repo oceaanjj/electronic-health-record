c1770795652c022d28f7f7d61d8cfc849b1f886b
oceaanjj <jovi.uccsc@gmail.com>
2025-09-16 22:24:48 +0800

    sidebar sample

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'home')->name('home');
 
 $views = [
+    'sidebar',
     'role',
     'medical-history',
     'physical-exam',
@@ -16,6 +17,7 @@ $views = [
     'lab-values',
     'diagnostics',
     'ivs-and-lines',
+
 ];
 
 foreach ($views as $view) {

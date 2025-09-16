397bf400989c979e3189e14eca2c6f5e34125a4b
Author <geroliabryan024@gmail.com>
2025-09-16 20:45:02 +0800

    setup the cdss physical exam model and private function of cdss in physical exam controller

--- app/Http/Controllers/PhysicalExamController.php
+++ app/Http/Controllers/PhysicalExamController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\PhysicalExam;
+use App\Models\CdssPhysicalExam;
 use Illuminate\Http\Request;
 
 class PhysicalExamController extends Controller
@@ -28,8 +29,20 @@ class PhysicalExamController extends Controller
         $physicalExam = PhysicalExam::create($data);
 
         // call the cdss method here and pass the $physicalExam data
+        $this->runCdssAnalysis($physicalExam);
 
-        //
+        // where itll go after storing
         return redirect()->route('physical_exams.index')->with('success', 'Physical exam registered successfully');
+    }
+
+    private function runCdssAnalysis($physicalExam)
+    {
+        // Build the if/else logic para sa cdss analysis
+        // [] If else sa bawat condition from general appearance to neurological (long ass shit)
+        // [] store it sa db
+        // [] return a view or redirect it
+
+
+
     }
 }

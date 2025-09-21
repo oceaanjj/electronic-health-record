6a5ceab825018af1983570d54f8de29c5b7f1384
Rex Parafina <parafinarex1@gmail.com>
2025-09-21 20:47:21 +0800

    VitalsProgress

--- app/Models/Vitals.php
+++ app/Models/Vitals.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vitals extends Model
 {
+    use HasFactory;
+
+
     protected $table = 'vital_signs';
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
 
     protected $fillable = [
         'patient_id',

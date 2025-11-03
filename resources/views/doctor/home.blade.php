99c5e5f771470bfcf0bb4b02374792d120aeed33
404keith <keithantonio444@gmail.com>
2025-11-04 00:13:30 +0800

    wip: doctors > generate patient result and save as pdf 2 table form (used partials) seperated files into doctors folder, fixed generate-report final?

--- resources/views/doctor/home.blade.php
+++ resources/views/doctor/home.blade.php
@@ -9,7 +9,7 @@
             <div class="bg-white shadow-md rounded-lg p-6">
                 <h2 class="text-xl font-semibold mb-4">Generate Patient Report</h2>
                 <a href="{{ route('doctor.patient-report') }}"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Generate Patient Report</a>
+                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Go</a>
             </div>
         </div>
     </div>

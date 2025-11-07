0f8a6e64a0c56cb0097d33ea112df310f0bd7656
404keith <keithantonio444@gmail.com>
2025-11-08 01:10:17 +0800

    pdf: format med-hi,dev-hi,pe,vs, adl, io. lv

--- resources/views/doctor/reports/partials/_intake_and_output.blade.php
+++ resources/views/doctor/reports/partials/_intake_and_output.blade.php
@@ -10,9 +10,8 @@
             <thead>
                 <tr>
                     <th>Day</th>
-                    <th>Date</th>
                     <th>Oral Intake</th>
-                    <th>IV Fluids Volume</th>
+                    <th>IV Fluids</th>
                     <th>Urine Output</th>
                     <th>Alert</th>
                 </tr>
@@ -21,7 +20,6 @@
                 @foreach($intakeAndOutput as $item)
                     <tr>
                         <td>{{ $item->day_no ?? 'N/A' }}</td>
-                        <td>{{ $item->date ?? 'N/A' }}</td>
                         <td>{{ $item->oral_intake ?? 'N/A' }}</td>
                         <td>{{ $item->iv_fluids_volume ?? 'N/A' }}</td>
                         <td>{{ $item->urine_output ?? 'N/A' }}</td>

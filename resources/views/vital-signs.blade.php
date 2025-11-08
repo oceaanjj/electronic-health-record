dd2586f86a9bbf83c5b4894cf480a6d39adf4547
404keith <keithantonio444@gmail.com>
2025-11-08 15:49:35 +0800

    taenam

--- resources/views/vital-signs.blade.php
+++ resources/views/vital-signs.blade.php
@@ -216,6 +216,8 @@
 @push('scripts')
 @vite(['resources/js/alert.js', 'resources/js/patient-loader.js', 'resources/js/searchable-dropdown.js'])
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<!-- VITAL SIGNS CHART -->
 <script>
 document.addEventListener("DOMContentLoaded", function () {
     const ctx = document.getElementById('vitalSignChart').getContext('2d');
@@ -335,6 +337,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     });
 });
 </script>
+<!-- VITAL SIGNS CHART -->
+
+
 @endpush
 
    

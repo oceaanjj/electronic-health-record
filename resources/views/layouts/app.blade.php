bb438d3373b66504fe6c84e35048815911039150
oceaanjj <jovi.uccsc@gmail.com>
2026-02-05 06:20:58 +0800

    loading fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Electronic Health Record</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="preload" as="image" href="img/ehr-logo.png">
+        <link rel="preload" as="image" href="img/loading.png">
 
         {{-- **google icons library nyhahhahaha** --}}
         <link

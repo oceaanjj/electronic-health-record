f32beb2a191b4438a35402ab164cd8d03e54997d
404keith <keithantonio444@gmail.com>
2026-02-18 15:04:38 +0800

    added all js into viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // CSS Files
                 'resources/css/app.css',
                 'resources/css/home-style.css',
                 'resources/css/login-style.css',
@@ -18,22 +19,37 @@ export default defineConfig({
                 'resources/css/search-style.css',
                 'resources/css/show-style.css',
 
-
+                // JS Files from Image
                 'resources/js/app.js',
+                'resources/js/adpie-alert.js',
                 'resources/js/alert.js',
-                'resources/js/date-day-loader.js',
-                'resources/js/patient-loader.js',
-                'resources/js/init-searchable-dropdown.js',
-                'resources/js/patient-search.js',
+                'resources/js/audit-search.js',
                 'resources/js/close-cdss-alert.js',
-                'resources/js/searchable-dropdown.js',
+                'resources/js/compute-age.js',
+                'resources/js/date-day-loader.js',
                 'resources/js/date-day-sync.js',
-                'resources/js/vital-signs-charts.js',
+                'resources/js/diagnostics.js',
+                'resources/js/discharge-planning-loader.js',
+                'resources/js/form-disable-alert.js',
+                'resources/js/form-saver.js',
+                'resources/js/init-searchable-dropdown.js',
+                'resources/js/intake-and-output-date-sync.js',
+                'resources/js/intake-output-cdss.js',
+                'resources/js/intake-output-data-loader.js',
+                'resources/js/intake-output-patient-loader.js',
                 'resources/js/medication-administration-loader.js',
                 'resources/js/medication-form-validation.js',
-                
+                'resources/js/page-initializer.js',
+                'resources/js/patient-loader.js',
+                'resources/js/patient-report.js',
+                'resources/js/patient-search.js',
+                'resources/js/search.js',
+                'resources/js/searchable-dropdown.js',
+                'resources/js/soft-delete.js',
+                'resources/js/sweetalert.js',
+                'resources/js/vital-signs-chart-updater.js',
+                'resources/js/vital-signs-charts.js',
             ],
-
             refresh: true,
         }),
         tailwindcss(),
